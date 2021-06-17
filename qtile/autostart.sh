51d3161dbbb752695ee22e8200dfee3d2bdcb37e
Martin Eldredge <martin@lappy.localdomain>
2021-06-17 13:08:24 -0600

    updated autostart script

--- qtile/autostart.sh
+++ qtile/autostart.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 # Called when qtile is started up
 
-picom &
+picom --config /home/martin/.config/picom/picom.conf &
 feh --bg-scale /home/martin/Pictures/wallpapers/valley.jpg &
+nm-applet &
+eval `ssh-agent -s` &
+ssh-add /home/martin/.ssh/github &
