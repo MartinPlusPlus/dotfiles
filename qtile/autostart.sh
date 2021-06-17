e3241bf2928ea7ee769e70bc4b18e955fecc14a0
Martin Eldredge <martin.eldredge1@gmail.com>
2021-06-17 14:51:38 -0600

    removed ssh-agent autostart

--- qtile/autostart.sh
+++ qtile/autostart.sh
@@ -4,5 +4,3 @@
 picom --config /home/martin/.config/picom/picom.conf &
 feh --bg-scale /home/martin/Pictures/wallpapers/valley.jpg &
 nm-applet &
-eval `ssh-agent -s` &
-ssh-add /home/martin/.ssh/github &
