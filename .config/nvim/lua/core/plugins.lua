1335cc676a614fd9d0ad09b7881389d6629e60f7
Martin Eldredge <martin.eldredge1@gmail.com>
2024-01-02 18:22:13 -0700

    added nvim autopairs

--- .config/nvim/lua/core/plugins.lua
+++ .config/nvim/lua/core/plugins.lua
@@ -56,6 +56,12 @@ return require('packer').startup(function(use)
             require("nvim-surround").setup({})
         end
     }
+    use {
+        "windwp/nvim-autopairs",
+        config = function() 
+            require("nvim-autopairs").setup {} 
+        end
+    }
     --
     -- Automatically set up your configuration after cloning packer.nvim
     -- Put this at the end after all plugins
