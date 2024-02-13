6fbad2821b7f676e4edf1048fe0ffd048e1f8023
Martin Eldredge <martin.eldredge1@gmail.com>
2024-02-12 21:21:44 -0700

    added rust lsp support

--- .config/nvim/lua/plugins/configs/lspconfig.lua
+++ .config/nvim/lua/plugins/configs/lspconfig.lua
@@ -7,18 +7,21 @@ local utils = require "core.utils"
 -- export on_attach & capabilities for custom lspconfigs
 
 M.on_attach = function(client, bufnr)
-  client.server_capabilities.documentFormattingProvider = false
-  client.server_capabilities.documentRangeFormattingProvider = false
+    client.server_capabilities.documentFormattingProvider = false
+    client.server_capabilities.documentRangeFormattingProvider = false
 
-  utils.load_mappings("lspconfig", { buffer = bufnr })
+    require("clangd_extensions.inlay_hints").setup_autocmd()
+    require("clangd_extensions.inlay_hints").set_inlay_hints()
 
-  if client.server_capabilities.signatureHelpProvider then
-    require("nvchad.signature").setup(client)
-  end
+    utils.load_mappings("lspconfig", { buffer = bufnr })
 
-  if not utils.load_config().ui.lsp_semantic_tokens and client.supports_method "textDocument/semanticTokens" then
-    client.server_capabilities.semanticTokensProvider = nil
-  end
+    if client.server_capabilities.signatureHelpProvider then
+        require("nvchad.signature").setup(client)
+    end
+
+    if not utils.load_config().ui.lsp_semantic_tokens and client.supports_method "textDocument/semanticTokens" then
+        client.server_capabilities.semanticTokensProvider = nil
+    end
 end
 
 M.capabilities = vim.lsp.protocol.make_client_capabilities()
@@ -69,6 +72,64 @@ require("lspconfig").pyright.setup {
     capabilities = M.capabilities,
 
     filetypes = {"python"},
+}
+
+require("lspconfig").rust_analyzer.setup {
+    on_attach = M.on_attach,
+    settings = {
+        ["rust_analyzer"] = {
+            imports = {
+                granularity = {
+                    group = "module",
+                },
+                prefix = "self"
+            },
+            cargo = {
+                buildScripts = {
+                    enable = true,
+                },
+            },
+            procMacro = {
+                enable = true
+            },
+        }
+    }
+}
+
+require("lspconfig").clangd.setup {
+    on_attach = M.on_attach,
+    inlay_hints = {
+        inline = vim.fn.has("nvim-0.10") == 1,
+        -- Options other than `highlight' and `priority' only work
+        -- if `inline' is disabled
+        -- Only show inlay hints for the current line
+        only_current_line = false,
+        -- Event which triggers a refresh of the inlay hints.
+        -- You can make this { "CursorMoved" } or { "CursorMoved,CursorMovedI" } but
+        -- not that this may cause  higher CPU usage.
+        -- This option is only respected when only_current_line and
+        -- autoSetHints both are true.
+        only_current_line_autocmd = { "CursorHold" },
+        -- whether to show parameter hints with the inlay hints or not
+        show_parameter_hints = true,
+        -- prefix for parameter hints
+        parameter_hints_prefix = "<- ",
+        -- prefix for all the other hints (type, chaining)
+        other_hints_prefix = "=> ",
+        -- whether to align to the length of the longest line in the file
+        max_len_align = false,
+        -- padding from the left if max_len_align is true
+        max_len_align_padding = 1,
+        -- whether to align to the extreme right or not
+        right_align = false,
+        -- padding from the right if right_align is true
+        right_align_padding = 7,
+        -- The color of the hints
+        highlight = "Comment",
+        -- The highlight group priority for extmark
+        priority = 100,
+    },
+    filetypes = {"c", "cpp", "cxx", "cc"},
 }
 
 return M
