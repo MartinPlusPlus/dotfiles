70ae2b1201a58660b8e1587cb2e61cc0295cdf14
Martin Eldredge <martin.eldredge1@gmail.com>
2025-01-08 18:27:07 -0700

    added 4 tabs spacing

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -3,3 +3,7 @@
 -- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
 -- Forum: https://www.reddit.com/r/lunarvim/
 -- Discord: https://discord.com/invite/Xb9B4Ny
+
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+vim.o.expandtab = true
