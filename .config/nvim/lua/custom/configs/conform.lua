cd5eff09766ad97a3b7dad96c33d23a5e0a51bfb
Martin Eldredge <martin.eldredge1@gmail.com>
2025-07-09 16:50:52 -0600

    got rid of format on save

--- .config/nvim/lua/custom/configs/conform.lua
+++ .config/nvim/lua/custom/configs/conform.lua
@@ -6,13 +6,13 @@ conform.setup({
         cpp = { "clang_format" },
         objc = { "clang_format" },
     },
-    format_on_save = function(bufnr)
-        local ft = vim.bo[bufnr].filetype
-        if ft == "c" or ft == "cpp" then
-            return { lsp_fallback = true }
-        end
-        return false
-    end,
+    -- format_on_save = function(bufnr)
+    --     local ft = vim.bo[bufnr].filetype
+    --     if ft == "c" or ft == "cpp" then
+    --         return { lsp_fallback = true }
+    --     end
+    --     return false
+    -- end,
 })
 
 vim.api.nvim_create_user_command("ConformFormat", function()
--- .config/nvim/lua/custom/plugins.lua
+++ .config/nvim/lua/custom/plugins.lua
@@ -7,15 +7,15 @@ local plugins = {
 
     {
         "neovim/nvim-lspconfig",
-        dependencies = {
-            -- format & linting
-            {
-                "jose-elias-alvarez/null-ls.nvim",
-                config = function()
-                    require "custom.configs.null-ls"
-                end,
-            },
-        },
+        -- dependencies = {
+        --     -- format & linting
+        --     {
+        --         "jose-elias-alvarez/null-ls.nvim",
+        --         config = function()
+        --             require "custom.configs.null-ls"
+        --         end,
+        --     },
+        -- },
         config = function()
             require "plugins.configs.lspconfig"
             require "custom.configs.lspconfig"
